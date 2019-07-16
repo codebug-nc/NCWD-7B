ac8b63983fb4d70d13a7d1bbad9503e392e56b65
camp user <camp@ma-15-camp-3.stlukesct.org>
2019-07-16 16:45:17 -0400

    monkey time

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
   // Write your JavaScript below
+  $("#secrets-box").hide()
+  $("#go-away-box").hide()
+
+
+$("#yes-secret").click(function() {
+
+ $("#buttons-box").hide()
+ $("#secrets-box").show()
+	
+});
+
+$("#No").click(function() {
+
+ $("#buttons-box").hide()
+ $("#go-away-box").show()
+	
+});
 
 
 });
